eea3ca3701037e204fd1d8d6ccec5f33054d5ad0
Nhoj <nhojplummer@yahoo.com>
2023-08-20 17:22:13 -0400

    Making some progress

--- src/components/Rating/rating.js
+++ src/components/Rating/rating.js
@@ -1,7 +1,9 @@
 import React from "react"
+import "./Rating.css"
 
 function Rating() {
-  return <div className="rating-container section-padding">Rating</div>
+  return <div className="rating-container section-padding"><div className="rating-container__initial">Initial</div>
+  <div className="rating-container__output">Output</div></div>
 }
 
 export default Rating
