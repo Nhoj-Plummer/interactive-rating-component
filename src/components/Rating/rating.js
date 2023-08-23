8d4ac8b6a1d5c4d205bf924cc905b1c97925d785
Nhoj <nhojplummer@yahoo.com>
2023-08-23 16:53:44 -0400

    Fixing a bit more css and adding js

--- src/components/Rating/rating.js
+++ src/components/Rating/rating.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Rating.css"
 import Card from "../Card/card"
 import Star from "../../assets/icon-star.svg"
 
 const Rating = () => {
+  const [selectChoice, setSelectChoice] = useState({
+    dataOne: false,
+    dataTwo: false,
+    dataThree: false,
+    dataFour: false,
+    dataFive: false
+  })
+
   return (
     <Card>
       <div className="rating-star_img_container">
@@ -12,19 +20,19 @@ const Rating = () => {
       <h2 className="rating-header-text">How did we do?</h2>
       <p className="rating-text">Please let us know how we did with your support request. All feedback is appreciated to help us imporve our offering!</p>
       <ul className="rating-number_container">
-        <li>
+        <li className={selectChoice.dataOne ? "rating-number active" : "rating-number_container-initial"}>
           <span className="rating-number">1</span>
         </li>
-        <li>
+        <li className="rating-number_container-initial">
           <span className="rating-number">2</span>
         </li>
-        <li>
+        <li className="rating-number_container-initial">
           <span className="rating-number">3</span>
         </li>
-        <li>
+        <li className="rating-number_container-initial">
           <span className="rating-number">4</span>
         </li>
-        <li>
+        <li className="rating-number_container-initial">
           <span className="rating-number">5</span>
         </li>
       </ul>
