cb653d4a4ade04cacbb96342cad86c1af7d22eaf
Nhoj <nhojplummer@yahoo.com>
2023-08-23 20:38:46 -0400

    Getting pass the pillars

--- src/components/Rating/rating.js
+++ src/components/Rating/rating.js
@@ -14,8 +14,7 @@ const Rating = (rating, setRating, setShowOutput) => {
 
   const handleSubmit = () => {
     if (!rating) return
-
-    setShowOutput(true)
+    
   }
 
   return (
@@ -24,21 +23,21 @@ const Rating = (rating, setRating, setShowOutput) => {
         <img src={Star} alt="star" />
       </div>
       <h2 className="rating-header-text">How did we do?</h2>
-      <p className="rating-text">Please let us know how we did with your support request. All feedback is appreciated to help us imporve our offering!</p>
+      <p className="rating-text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       <ul className="rating-number_container">
-        <li className={selectChoice.dataOne ? "rating-number active" : "rating-number_container-initial"} onClick={(set) => setSelectChoice({ dataOne: true, dataTwo: false, dataThree: false, dataFour: false, dataFive: false })}>
+        <li className={selectChoice.dataOne ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: true, dataTwo: false, dataThree: false, dataFour: false, dataFive: false }, () => setRating(1))}>
           <span className="rating-number">1</span>
         </li>
-        <li className={selectChoice.dataTwo ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: true, dataThree: false, dataFour: false, dataFive: false })}>
+        <li className={selectChoice.dataTwo ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: true, dataThree: false, dataFour: false, dataFive: false }, () => setRating(2))}>
           <span className="rating-number">2</span>
         </li>
-        <li className={selectChoice.dataThree ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: true, dataFour: false, dataFive: false })}>
+        <li className={selectChoice.dataThree ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: true, dataFour: false, dataFive: false }, () => setRating(3))}>
           <span className="rating-number">3</span>
         </li>
-        <li className={selectChoice.dataFour ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: false, dataFour: true, dataFive: false })}>
+        <li className={selectChoice.dataFour ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: false, dataFour: true, dataFive: false }, () => setRating(4))}>
           <span className="rating-number">4</span>
         </li>
-        <li className={selectChoice.dataFive ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: false, dataFour: false, dataFive: true })}>
+        <li className={selectChoice.dataFive ? "rating-number active" : "rating-number_container-initial"} onClick={() => setSelectChoice({ dataOne: false, dataTwo: false, dataThree: false, dataFour: false, dataFive: true }, () => setRating(5))}>
           <span className="rating-number">5</span>
         </li>
       </ul>
